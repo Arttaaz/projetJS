dfdbc14a950117e8bc8815522787209bbfb3e9a8
Alexpert <salmon.alexandre10@gmail.com>
2018-03-23 09:05:13 +0100

    localStorage

--- news.js
+++ news.js
@@ -5,60 +5,63 @@ var recherche_courante_news=[]; // tableau d'objets de type resultats (avec titr
 function ajouter_recherche()
 {
 
+
+	var recherche = document.getElementById("zone_saisie");
+	recherches.push(recherche);
+	localStorage.recherches = JSON.stringify(recherches);
 }
 
 function supprimer_recherche(e)
-{ 
+{
+	
 
 
+	var rechercheASuppr = $('.titre-recherche > label').text();
+
+	for (var i = 0; i < recherches.length; i++) {
+		if (recherches[i] == rechercheASuppr) {
+			recherches.splice(i - 1, 1);
+			localStorage.recherches = JSON.stringify(recherches);
+		}
+	}
 }
 
 
 function selectionner_recherche(e)
-{ 
+{
 
 }
 
 
 function init()
 {
-
+	if (localStorage.recherches) {
+		recherches = JSON.parse(localStorage.recherches);
+	}
 }
 
 
 function rechercher_nouvelles()
 {
-	
-	
+
+
 }
 
 
 function maj_resultats(res)
 {
 
-	
+
 }
 
 
 function sauver_nouvelle(e)
 {
-	
+
 }
 
 
 function supprimer_nouvelle(e)
 {
-	
+
 }
-
-
-
-
-
-	
-
-
-
-
-
-
